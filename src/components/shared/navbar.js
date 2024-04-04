6b8358ea0a907a4d07a04946c3588fa1f5d58cd3
NComDev <ncomdev@proton.me>
2024-04-04 17:59:09 -0400

    Added MUI package; Modified Navbar to use MUI

--- src/components/shared/navbar.js
+++ src/components/shared/navbar.js
@@ -1,13 +1,42 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
-import Modal from "react-modal";
+import { AppBar, Toolbar, Typography, Button, Modal, Box, TextField, Menu, MenuItem } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import { Link as RouterLink } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const ModalBox = styled(Box)({
+  position: 'absolute',
+  width: 400,
+  backgroundColor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  padding: '16px 32px 24px',
+});
 
 const Navbar = ({ username }) => {
   const [isDropdownVisible, setDropdownVisible] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [mosaicName, setMosaicName] = useState("");
+
+  const handleOpenDropdown = (event) => {
+    setDropdownVisible(event.currentTarget);
+  };
+
+  const handleCloseDropdown = () => {
+    setDropdownVisible(null);
+  };
 
   async function createMosaic(e) {
     e.preventDefault();
@@ -18,75 +47,75 @@ const Navbar = ({ username }) => {
       await axios
         .post(`${baseUrl}/mosaics/create`, {
           title: mosaicName,
-          owner: "slough", //change this to JWT user
+          owner: "slough", // Change this to JWT user
         })
         .then((res) => {
-          if (res.data == "success") {
+          if (res.data === "success") {
             console.log("Mosaic created");
           } else if (res.status === 400) {
-            console.log("bad bad things");
+            console.log("Bad request");
           }
         })
-        .catch((e) => {
-          console.log(e);
+        .catch((error) => {
+          console.log(error);
         });
-    } catch (e) {
-      console.log(e);
+    } catch (error) {
+      console.log(error);
     }
   }
 
   return (
-    <nav className="navbar">
-      <div className="navbar-links">
-        <Link to="/home" className="nav-title" id="nav-title">
-          ProjecTile
-        </Link>
-        <Link to="/mosaics" className="nav-link">
-          My Mosaics
-        </Link>
-        <button onClick={() => setModalIsOpen(true)}>New Mosaic</button>
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={() => setModalIsOpen(false)}
-          className="modal"
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button color="inherit" component={RouterLink} to="/home">ProjecTile</Button>
+        </Typography>
+        <Button color="inherit" component={RouterLink} to="/mosaics">My Mosaics</Button>
+        <Button color="inherit" onClick={() => setModalIsOpen(true)}>New Mosaic</Button>
+        <Button color="inherit" onClick={handleOpenDropdown}>{username}</Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={isDropdownVisible}
+          keepMounted
+          open={Boolean(isDropdownVisible)}
+          onClose={handleCloseDropdown}
         >
-          <label>Create a new mosaic</label>
-          <br />
-          <input
-            type="text"
-            value={mosaicName}
-            onChange={(e) => setMosaicName(e.target.value)}
-            placeholder="Enter Mosaic Name"
-          />
-          <button
-            onClick={(e) => {
-              createMosaic(e);
-              setModalIsOpen(false);
-            }}
-          >
-            Continue
-          </button>
-          <button onClick={() => setModalIsOpen(false)}>Cancel</button>
-        </Modal>
-        <div
-          className="nav-item"
-          onMouseEnter={() => setDropdownVisible(true)}
-          onMouseLeave={() => setDropdownVisible(false)}
+          <MenuItem onClick={handleCloseDropdown} component={RouterLink} to="/settings">Settings</MenuItem>
+          <MenuItem onClick={handleCloseDropdown} component={RouterLink} to="/logout">Logout</MenuItem>
+        </Menu>
+        <StyledModal
+          open={modalIsOpen}
+          onClose={() => setModalIsOpen(false)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
         >
-          <span className="nav-link">{username}</span>
-          {isDropdownVisible && (
-            <div className="nav-dropdown">
-              <Link to="/settings" className="nav-dropdown-item">
-                Settings
-              </Link>
-              <Link to="/logout" className="nav-dropdown-item">
-                Logout
-              </Link>
-            </div>
-          )}
-        </div>
-      </div>
-    </nav>
+          <ModalBox>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Create a new mosaic
+            </Typography>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="mosaicName"
+              label="Mosaic Name"
+              name="mosaicName"
+              autoComplete="mosaicName"
+              autoFocus
+              value={mosaicName}
+              onChange={(e) => setMosaicName(e.target.value)}
+            />
+            <Button onClick={(e) => { createMosaic(e); setModalIsOpen(false); }} color="primary">
+              Continue
+            </Button>
+            <Button onClick={() => setModalIsOpen(false)} color="secondary">
+              Cancel
+            </Button>
+          </ModalBox>
+        </StyledModal>
+        {/* Dropdown logic can be implemented with MUI's Menu component */}
+      </Toolbar>
+    </AppBar>
   );
 };
 
