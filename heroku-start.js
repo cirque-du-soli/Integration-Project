a14583fb93524fd799966b7cfd998edc09003e2d
Soliloquy Yarrow <soliloquy.yarrow@gmail.com>
2024-04-13 03:52:22 -0400

    Update heroku-start.js

--- heroku-start.js
+++ heroku-start.js
@@ -14,5 +14,5 @@ app.get('*', function(req,res){
   res.sendFile(path.join(__dirname, '/', 'build/index.html'));
 });
 app.listen(port, ()=>{
-  console.log("Server is running on port: ", port)
+  console.log("S0011 Server is running on port: ", port)
 })
