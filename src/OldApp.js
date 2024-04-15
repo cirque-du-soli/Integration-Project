32d85bea6482ddaa60b77354bd8703279f9da926
Soliloquy Yarrow <soliloquy.yarrow@gmail.com>
2024-04-15 10:45:09 -0400

    fixed links

--- src/OldApp.js
+++ src/OldApp.js
@@ -1,19 +1,6 @@
-import { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import axios from "axios";
-import { AuthContext } from "./contexts/authContext.js";
-
 // STYLE imports
 import "./styles/tailwind.css";
 import "./styles/App.css";
-
-
-import { NavigateBefore } from "@mui/icons-material";
 
 function App() {
 
@@ -23,56 +10,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
       </header>
-    </div>
-  );
-
-  //check if logged in
-  const [authState, setAuthState] = useState(false);
-  const [userState, setUserState] = useState("");
-  
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API_BASE_URL}/auth`, {
-        headers: { accessToken: localStorage.getItem("accessToken") },
-      })
-      .then((response) => {
-        if (response.data.error) {
-          setAuthState(false);
-        } else {
-          console.log(response.data);
-          setUserState(response.data);
-          setAuthState(true);
-        }
-      });
-  }, [localStorage.getItem("accessToken")]);
-
-  //for selected mosaic
-  const [selMosaic, setSelMosaic] = useState("");
-
-  return (
-    <div className="App">
-      <AuthContext.Provider
-        value={{
-          authState,
-          setAuthState,
-          userState,
-          setUserState,
-          selMosaic,
-          setSelMosaic,
-        }}
-      >
-        <Router>
-          <Routes>
-            <Route path="/" element={<Navigate to={authState ? "/home" : "/login" }/>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/regi" element={<Regi />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/*" element={<PageNotFound />} />
-            <Route path="/main" element={<Main />} />
-          </Routes>
-        </Router>
-      </AuthContext.Provider>
     </div>
   );
 }
