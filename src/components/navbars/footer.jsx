1f68fee8ffd9726f342e94b0b7d080c4f803d664
DrSloughKeg <bpye1995@gmail.com>
2024-04-22 13:48:47 -0400

    bug fix, pathway

--- src/components/navbars/footer.jsx
+++ src/components/navbars/footer.jsx
@@ -1,46 +1,57 @@
-import { React, useState, useRef, useContext } from 'react';
-import { Button } from 'reactstrap';
-import AdminToggleButton from "../admin/AdminToggleButton.jsx";
-import { newToastMessage } from '../customToast.js';
+import { React, useState, useRef, useContext } from "react";
+import { Button } from "reactstrap";
+import AdminToggleButton from "../admin/adminToggleButton.jsx";
+import { newToastMessage } from "../customToast.js";
 import { AuthContext } from "../../contexts/authContext.js";
 
 function Footer({ props }) {
+  const mainPanelRef = useRef(null);
 
-    const mainPanelRef = useRef(null);
+  // initial states
+  const [isAdmin, setIsAdmin] = useState(props.userAdminState);
+  localStorage.setItem("isAdmin", isAdmin);
 
-    // initial states
-    const [isAdmin, setIsAdmin] = useState(props.userAdminState);
-    localStorage.setItem('isAdmin', isAdmin);
+  function toggleIsAdmin() {
+    // update storage
+    //localStorage.setItem('isAdmin', (localStorage.getItem('isAdmin') === 'true') ? 'false' : 'true');
+    // update state (re-renders div)
+    // setIsAdmin(localStorage.getItem('isAdmin'));
 
-    function toggleIsAdmin() {
-        // update storage
-        //localStorage.setItem('isAdmin', (localStorage.getItem('isAdmin') === 'true') ? 'false' : 'true');
-        // update state (re-renders div)
-        // setIsAdmin(localStorage.getItem('isAdmin'));
+    props.setUserAdminState(!props.userAdminState);
+    // setIsAdmin(userAdminState);
+    localStorage.setItem("isAdmin", props.userAdminState);
+  }
 
-        props.setUserAdminState(!props.userAdminState);
-        // setIsAdmin(userAdminState);
-        localStorage.setItem('isAdmin', props.userAdminState);
-    }
+  return (
+    <footer className="bg-teal-500 text-white py-4 fixed bottom-0 w-full">
+      <div className="container mx-auto flex flex-wrap justify-between items-center font-semibold">
+        <div className="w-full md:w-auto mb-4 md:mb-0">
+          <a href="/" className="text-black hover:text-gray-400 mr-4">
+            Home
+          </a>
+          <span className="text-black">|</span>
+          <a href="#" className="text-black hover:text-gray-400 ml-4 mr-4">
+            Terms of Service
+          </a>
+          <span className="text-black">|</span>
+          <a href="#" className="text-black hover:text-gray-400 ml-4">
+            Privacy Policy
+          </a>
+          <span className="text-black"> | </span>
+          <AdminToggleButton
+            props={{
+              isAdmin: props.userAdminState,
+              toggleIsAdmin: toggleIsAdmin,
+              newToastMessage: newToastMessage,
+            }}
+          />
+        </div>
 
-    return (
-        <footer className="bg-teal-500 text-white py-4 fixed bottom-0 w-full">
-            <div className="container mx-auto flex flex-wrap justify-between items-center font-semibold">
-                <div className="w-full md:w-auto mb-4 md:mb-0">
-                    <a href='/' className="text-black hover:text-gray-400 mr-4">Home</a>
-                    <span className="text-black">|</span>
-                    <a href='#' className="text-black hover:text-gray-400 ml-4 mr-4">Terms of Service</a>
-                    <span className="text-black">|</span>
-                    <a href='#' className="text-black hover:text-gray-400 ml-4">Privacy Policy</a>
-                    <span className="text-black"> | </span>
-                    <AdminToggleButton props={{ isAdmin: props.userAdminState, toggleIsAdmin: toggleIsAdmin, newToastMessage: newToastMessage}} />
-                </div>
-
-                <div className="w-full md:w-auto text-center md:text-right">
-                    <p className="text-black">© 2024: Team 1 Web Development Inc.</p>
-                </div>
-            </div>
-        </footer>
-    );
+        <div className="w-full md:w-auto text-center md:text-right">
+          <p className="text-black">© 2024: Team 1 Web Development Inc.</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 export default Footer;
--- src/layouts/devLayout.jsx
+++ src/layouts/devLayout.jsx
@@ -1,53 +1,61 @@
 // IMPORT: React
 import { Route, Routes, Navigate } from "react-router-dom";
-import { useState, useRef, React } from 'react';
+import { useState, useRef, React } from "react";
 
 // IMPORT: Routes
 import devRoutes from "../routes/devRoutes.js";
 
 // IMPORT: Components & Functions
-import AdminToggleButton from "../components/admin/AdminToggleButton.jsx";
-import { newToastMessage } from '../components/customToast.js';
+import AdminToggleButton from "../components/admin/adminToggleButton.jsx";
+import { newToastMessage } from "../components/customToast.js";
 
 const getRoutes = (routes) => {
-
-    return routes.map((prop, key) => {
-        return <Route path={prop.path} element={prop.component} key={key} exact />
-    });
+  return routes.map((prop, key) => {
+    return <Route path={prop.path} element={prop.component} key={key} exact />;
+  });
 };
 
 function DevLayout(props) {
+  const mainPanelRef = useRef(null);
 
-    const mainPanelRef = useRef(null);
+  // initial states
+  const [isAdmin, setIsAdmin] = useState("true");
+  localStorage.setItem("isAdmin", isAdmin);
 
-    // initial states
-    const [isAdmin, setIsAdmin] = useState('true');
-    localStorage.setItem('isAdmin', isAdmin);
-
-    function toggleIsAdmin() {
-        // update storage
-        localStorage.setItem('isAdmin', (localStorage.getItem('isAdmin') === 'true') ? 'false' : 'true');
-        // update state (re-renders div)
-        setIsAdmin(localStorage.getItem('isAdmin'));
-    }
-
-    return (
-        <>
-            <div className="App">
-                <div className="main-panel" ref={mainPanelRef}>
-                    <Routes>
-                        {getRoutes(devRoutes)}
-                        <Route path="/*" element={<Navigate to='/dev/dashboard' replace />} />
-                    </Routes>
-
-                    <AdminToggleButton props={{ toggleIsAdmin: toggleIsAdmin, newToastMessage: newToastMessage, isAdmin: isAdmin }} />
-                    <p>isAdmin: {isAdmin}</p>
-
-                </div>
-            </div>
-        </>
+  function toggleIsAdmin() {
+    // update storage
+    localStorage.setItem(
+      "isAdmin",
+      localStorage.getItem("isAdmin") === "true" ? "false" : "true"
     );
+    // update state (re-renders div)
+    setIsAdmin(localStorage.getItem("isAdmin"));
+  }
+
+  return (
+    <>
+      <div className="App">
+        <div className="main-panel" ref={mainPanelRef}>
+          <Routes>
+            {getRoutes(devRoutes)}
+            <Route
+              path="/*"
+              element={<Navigate to="/dev/dashboard" replace />}
+            />
+          </Routes>
+
+          <AdminToggleButton
+            props={{
+              toggleIsAdmin: toggleIsAdmin,
+              newToastMessage: newToastMessage,
+              isAdmin: isAdmin,
+            }}
+          />
+          <p>isAdmin: {isAdmin}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default DevLayout;
-
